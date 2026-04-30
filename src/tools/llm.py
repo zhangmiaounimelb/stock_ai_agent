d881023cda09e85463f86c6b629a6cded54ca0c9
Miao Zhang <zhangmiaounimelb@gmail.com>
2026-04-30 21:30:52 +0200

    created call_llm interface and the call function of deepseek. a unit test is created for the call_llm function

---
+++ src/tools/llm.py
@@ -0,0 +1,35 @@
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def deepseek(prompt: str) -> str:
+    import httpx
+    api = os.getenv("DEEPSEEK_API_KEY")
+
+    response = httpx.post(
+        "https://api.deepseek.com/chat/completions",
+        headers= {"Authorization": f"Bearer {api}",
+                 "Content-Type": "application/json"},
+        json={
+            "model": "deepseek-3.5",
+            "messages": [{"role": "user", "content": prompt}]
+            }
+    )    
+
+    response.raise_for_status()
+    return response.json()["choices"][0]["message"]["content"]
+
+_PROVIDERS = {
+    "deepseek": deepseek,
+}
+
+def call_llm(prompt: str, provider: str = "deepseek") -> str:
+
+    if provider not in _PROVIDERS:
+        raise ValueError(f"Unsupported provider: {provider}")
+    
+    return _PROVIDERS[provider](prompt)
+
+
+ 
---
+++ tests/unit/test_llm.py
@@ -0,0 +1,5 @@
+from src.tools.llm import call_llm
+
+res = call_llm("用一句话介绍茅台公司")
+print(res) 
+
